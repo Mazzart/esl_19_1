bffdd2577076bae2ef36e802561b8ded0361b169
Mazzart <am.magicsmile@gmail.com>
2019-05-21 14:53:21 +0300

    Add Document class with corresponding fields and custom save and delete methods

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Document(models.Model):
+    """Class for document fields"""
+
+    file = models.FileField(upload_to='documents/')
+    time = models.IntegerField(default=0)
+    create_date = models.DateTimeField(default=timezone.now, blank=True)
+    end_date = models.DateTimeField()
+
+    @property
+    def get_time_end(self):
+        """Calculate the expiration date of the file"""
+
+        if self.create_date:
+            end = self.create_date + timezone.timedelta(seconds=self.time)
+            return end
+
+    def save(self, *args, **kwargs):
+        """Custom save method"""
+
+        self.end_date = self.get_time_end
+        super(Document, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """Custom delete method"""
+
+        self.file.delete()
+        super().delete(*args, **kwargs)
